a775268853f8ab4034b3f061019fa3f8625ca43b
jefersonmatsui <jefersonmatsui@gmail.com>
2022-09-27 19:26:13 -0300

    Atualização

---
+++ 01 - Python para Cientista de Dados/03. Tipos de Operadores com Python/09_ operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x = (10 + 5 * 4)
+print(x)
+
+z = (10 + 5) * 4
+print(z)
+
+y = (10 / 2 + 25 * 2 - 2 ** 2)
+print(y)
+
+a = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(a)
---
+++ 01 - Python para Cientista de Dados/03. Tipos de Operadores com Python/14_operadores_de_associacao.py
@@ -0,0 +1,7 @@
+frutas = ['limão', 'uva']
+curso = 'Curso de Python'
+
+print('laranja' in frutas)
+print('limão' not in frutas)
+print('Python' in curso)
+
