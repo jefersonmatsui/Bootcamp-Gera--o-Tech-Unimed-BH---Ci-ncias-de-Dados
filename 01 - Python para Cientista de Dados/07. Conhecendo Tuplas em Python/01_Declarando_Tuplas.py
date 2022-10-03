53d1e8c302f989307630d6c425c04f39f73ba99d
jefersonmatsui <jefersonmatsui@gmail.com>
2022-10-03 19:05:19 -0300

    Atualizado

---
+++ 01 - Python para Cientista de Dados/07. Conhecendo Tuplas em Python/01_Declarando_Tuplas.py
@@ -0,0 +1,11 @@
+frutas = ("laranja", "pera", "uva",) # Vírgula no final declaração de tuplas
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
